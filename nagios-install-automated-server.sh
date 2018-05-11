3cfdc2f4125be002e85c59abc97d939d6a458c96
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-05-11 11:59:33 -0700

    Update nagios-install-automated-server.sh

--- nagios-install-automated-server.sh
+++ nagios-install-automated-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [ -e /etc/nagios/objects/commands.cfg ]; then exit 0; fi
 yum install nagios -y
 systemctl enable nagios
 systemctl start nagios
@@ -13,6 +14,15 @@ yum -y install nagios-plugins-nrpe
 yum install -y nagios-selinux -y
 systemctl restart nagios
 setenforce 1
+
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.142.0.7/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo 
 
 echo '########### NRPE CONFIG LINE #######################
 define command{
