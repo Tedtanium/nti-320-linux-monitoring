95ee945d63207be7fdc397bb6707a6dbcf2d4305
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-11 11:48:26 -0700

    Create nagios.sh

---
+++ automated-network/nagios.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -e /etc/nagios/objects/commands.cfg ]; then exit 0; fi
+
+yum install nagios nrpe nagios-plugins-all nagios-plugins-nrpe nagios-selinux httpd -y
+
+
+
+systemctl enable nagios
+systemctl start nagios
+systemctl enable nrpe
+systemctl start nrpe
+
+
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://REPOSERVERIP/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo 
+
+echo '########### NRPE CONFIG LINE #######################
+define command{
+command_name check_nrpe
+command_line $USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
+}' >> /etc/nagios/objects/commands.cfg
+
+echo "*.info;mail.none;authpriv.none;cron.none   @RSYSLOGIP" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
+systemctl restart nagios
