426b91ef4f60371293b28e1b2e1c57e693fe752f
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-11 13:29:57 -0700

    Update master.sh

--- automated-network/master.sh
+++ automated-network/master.sh
@@ -15,6 +15,24 @@ nti-320-200300
 yum install git -y
 git clone https://github.com/Tedtanium/nti-320-linux-monitoring.git
 
+####### INSTANCE STARTUP BUILD REGION ########
+
+
+#For loop - Runs through a gotten list (for line in) - /nti-320-linux-monitoring/automated-network/configs/instances-list
+  gcloud compute instances create $HOSTNAME	--metadata-from-file startup-script=NTI-320-linux-monitoring/automated-network/$HOSTNAME.sh --image centos-7 --tags http-server --zone us-east1-b --machine-type f1-micro 	--scopes cloud-platform 
+
+  IP=$(getent hosts <servername>.c.nti-320-200300.internal | awk '{ print $1 }')
+#Runs the Nagios client creation script on the Nagios server, passing hostname and IP variables as arguments.
+  gcloud compute ssh root@nagios --command "bash /generate-nagios-client.sh $HOSTNAME $IP"
+  
+  #if statements go here - Checks for conditions, primarily what $HOSTNAME is, and checks it against whether or not it is a
+  #hostname that requires an IP to be extracted, or pushed in somewhere.
+
+
+###########################################
+
+
+
 ###########################################
 ############ NTI-310 SECTION ##############
 ###########################################
