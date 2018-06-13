9a94b6236796b135df05eacd8b75ec4e28700c03
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-12 19:08:27 -0700

    Update rsyslog.sh

--- automated-network/rsyslog.sh
+++ automated-network/rsyslog.sh
@@ -9,3 +9,13 @@ sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
 systemctl restart rsyslog
 
 #For clients: echo "*.info;mail.none;authpriv.none;cron.none   @<IP ADDR>" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
+#Adds repo to known repositories.
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.142.0.7/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo   
