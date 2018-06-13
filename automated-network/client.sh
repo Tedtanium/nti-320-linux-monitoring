c677536e78586f283f93b8c16e4bbe0126133c0d
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-12 19:09:38 -0700

    Update client.sh

--- automated-network/client.sh
+++ automated-network/client.sh
@@ -35,8 +35,17 @@ sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
 #To test: Go into the client and use [getent passwd | grep 500].
 
 #NFS client starts here.
-
 apt-get install nfs-client -y
 yum install nfs-client -y
 mkdir /mnt/test 
 echo "NFSIP:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+
+#Adds repo to known repositories.
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.142.0.7/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo   
